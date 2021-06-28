6638c040a13cbf371593af05fcf13cba8670331b
Chris Mendez Pop OS <cmendez20@utexas.edu>
2021-06-27 23:07:14 -0500

    Added foundamentals 04 exercise files

---
+++ js_basics/js_foundations_04/playground.js
@@ -0,0 +1,21 @@
+let i = 0;
+while (++i < 5) alert(i);
+
+/*
+OUTPUT: 
+        1
+        2
+        3 
+        4
+*/
+
+let i = 0;
+while (i++ < 5) alert(i);
+/*
+OUTPUT: 
+        0
+        1
+        2 
+        3
+        4
+*/
