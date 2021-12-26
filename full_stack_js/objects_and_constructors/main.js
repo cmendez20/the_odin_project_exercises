d346d2fca1e0eea2b836e72b30a82ddef0237c88
Chris Mendez Pop OS <cmendez20@utexas.edu>
2021-12-26 00:48:35 -0600

    Add book to library function with submit event handler

--- full_stack_js/objects_and_constructors/main.js
+++ full_stack_js/objects_and_constructors/main.js
@@ -1,44 +1,21 @@
 const submitButton = document.querySelector('#btnsubmit');
 const form = document.querySelector('.form');
+const card = document.querySelector('.card');
 
-const myLibrary = [
-  {
-    title: 'Book Title',
-    author: 'Chris Mendez',
-    pages: '420',
-    read: true,
-  },
-  {
-    title: 'Book Title 2',
-    author: 'Chris Mendez The 2nd',
-    pages: '724',
-    read: false,
-  },
-  {
-    title: 'Book Title 3',
-    author: 'Chris Mendez The 3rd',
-    pages: '720',
-    read: true,
-  },
-];
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${this.title} by ${this.author}. ${this.pages} pages, ${
-      this.read ? 'read' : 'not read yet'
-    }.`;
-  };
 }
 
 function addBookToLibrary() {
-  const bookTitleValue = document.querySelector('#form-input--title').value;
-  const bookAuthorValue = document.querySelector('#form-input--author').value;
-  const bookPagesValue = document.querySelector('#form-input--pages').value;
-  const bookReadValue = document.querySelector('#form-input--read').value;
+  const bookTitleValue = form.title.value;
+  const bookAuthorValue = form.author.value;
+  const bookPagesValue = form.pageCount.value;
+  const bookReadValue = form.read.value;
 
   let userBook = new Book(
     bookTitleValue,
@@ -47,38 +24,59 @@ function addBookToLibrary() {
     bookReadValue
   );
 
-  console.log(userBook);
   myLibrary.push(userBook);
 
   renderLibrary(myLibrary);
+  clearInput();
 }
 
 const renderLibrary = myLibrary => {
-  let index = myLibrary.length - 1;
-  let book = myLibrary[index];
-  console.log(book);
-  let div = document.createElement('div');
-  div.innerHTML = `
-  <h2>${book.title}</h2>
-  <p>Author: ${book.author}</p>
-  <p>Page Count: ${book.pages}</p>
-  <p>Book read? ${book.read}</p>
-  `;
-  document.body.append(div);
+  // let index = myLibrary.length - 1;
+  // let book = myLibrary[index];
+
+  // let div = document.createElement('div');
+  // div.setAttribute('data-index', index);
+  // div.classList.add('card');
+
+  // div.innerHTML = `
+  // <h2>${book.title}</h2>
+  // <p>Author: ${book.author}</p>
+  // <p>Page Count: ${book.pages}</p>
+  // <p>Book read? ${book.read}</p>
+  // <button class="btn btn--remove" data-type="removeBtn">Remove book</button>
+  // `;
+  // document.body.append(div);
+
+  myLibrary.forEach((book, index) => {
+    let div = document.createElement('div');
+    div.setAttribute('data-index', index);
+    div.classList.add('card');
+
+    div.innerHTML = `
+    <h2>${book.title}</h2>
+    <p>Author: ${book.author}</p>
+    <p>Page Count: ${book.pages}</p>
+    <p>Book read? ${book.read}</p>
+    <button class="btn btn--remove" data-type="removeBtn">Remove book</button>
+    `;
+    document.body.append(div);
+  });
 };
 
-// myLibrary.forEach(book => {
-//   let div = document.createElement('div');
-//   div.innerHTML = `
-//     <h2>${book.title}</h2>
-//     <p>Author: ${book.author}</p>
-//     <p>Page Count: ${book.pages}</p>
-//     <p>Book read? ${book.read}</p>
-//     `;
-//   document.body.append(div);
-// });
+const clearInput = function () {
+  form.reset();
+};
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   addBookToLibrary();
+});
+
+document.addEventListener('click', e => {
+  if (e.target.dataset.type === 'removeBtn') {
+    const parentDiv = e.target.parentElement;
+    const parentIndex = parentDiv.dataset.index;
+    myLibrary.splice(parentIndex, 1);
+    renderLibrary(myLibrary);
+  }
 });
