14b01896fee42e8bd64a1876b506727ef839e984
Chris Mendez Pop OS <cmendez20@utexas.edu>
2021-12-25 21:38:45 -0600

    Working through library project

--- full_stack_js/objects_and_constructors/main.js
+++ full_stack_js/objects_and_constructors/main.js
@@ -1,4 +1,28 @@
-function Books(title, author, pages, read) {
+const submitButton = document.querySelector('#btnsubmit');
+const form = document.querySelector('.form');
+
+const myLibrary = [
+  {
+    title: 'Book Title',
+    author: 'Chris Mendez',
+    pages: '420',
+    read: true,
+  },
+  {
+    title: 'Book Title 2',
+    author: 'Chris Mendez The 2nd',
+    pages: '724',
+    read: false,
+  },
+  {
+    title: 'Book Title 3',
+    author: 'Chris Mendez The 3rd',
+    pages: '720',
+    read: true,
+  },
+];
+
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -10,5 +34,51 @@ function Books(title, author, pages, read) {
   };
 }
 
-const hobbit = new Books('The Hobbit', 'J.R.R. Tolkien', 295, false);
-console.log(hobbit.info());
+function addBookToLibrary() {
+  const bookTitleValue = document.querySelector('#form-input--title').value;
+  const bookAuthorValue = document.querySelector('#form-input--author').value;
+  const bookPagesValue = document.querySelector('#form-input--pages').value;
+  const bookReadValue = document.querySelector('#form-input--read').value;
+
+  let userBook = new Book(
+    bookTitleValue,
+    bookAuthorValue,
+    bookPagesValue,
+    bookReadValue
+  );
+
+  console.log(userBook);
+  myLibrary.push(userBook);
+
+  renderLibrary(myLibrary);
+}
+
+const renderLibrary = myLibrary => {
+  let index = myLibrary.length - 1;
+  let book = myLibrary[index];
+  console.log(book);
+  let div = document.createElement('div');
+  div.innerHTML = `
+  <h2>${book.title}</h2>
+  <p>Author: ${book.author}</p>
+  <p>Page Count: ${book.pages}</p>
+  <p>Book read? ${book.read}</p>
+  `;
+  document.body.append(div);
+};
+
+// myLibrary.forEach(book => {
+//   let div = document.createElement('div');
+//   div.innerHTML = `
+//     <h2>${book.title}</h2>
+//     <p>Author: ${book.author}</p>
+//     <p>Page Count: ${book.pages}</p>
+//     <p>Book read? ${book.read}</p>
+//     `;
+//   document.body.append(div);
+// });
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  addBookToLibrary();
+});
